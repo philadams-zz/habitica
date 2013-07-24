d199f56103d8e636dd63ff9c08214b98ede53d97
Phil Adams <phil@philadams.net>
2013-07-23 22:02:38 -0700

    spec out interface to API GET endpoints

--- hrpg/core.py
+++ hrpg/core.py
@@ -9,9 +9,33 @@ http://github.com/philadams/hrpg
 """
 
 
+from docopt import docopt
+
+
 def cli():
-    pass
+    """HabitRPG command-line interface.
+
+    usage:
+      hrpg status
+      hrpg user [<uid>]
+      hrpg tasks (--habit | --daily | --todo | --reward) [<uid>]
+      hrpg task <tid> [<uid>]
+      hrpg
+      hrpg --version
+
+    options:
+      -h --help     Show this screen.
+      --version     Show version.
+
+    Subcommands:
+      status        Get status of HabitRPG service
+      user          Get user <uid> status
+      tasks         Get user <uid> tasks
+      task          Get task <tid> details
+      <none>        Get user status for this user (convenience)
+    """
+    args = docopt(cli.__doc__, version='hrpg version 0.0.2')
 
 
-if '__main__' == __name__:
+if __name__ == '__main__':
     cli()

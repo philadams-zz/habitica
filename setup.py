1d1909b7ace52ec935bd0560745a3d109a55e606
Phil Adams <philadams.net@gmail.com>
2015-01-05 17:16:33 -0500

    include README.md in distribution.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='hrpg',
-    version='0.0.6',
+    version='0.0.7',
     author='Phil Adams',
     author_email='philadams.net@gmail.com',
     url='https://github.com/philadams/hrpg',

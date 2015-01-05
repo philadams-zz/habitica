c6406e9a7e196f1a3e7c9157bb33648b420e4d1d
Phil Adams <philadams.net@gmail.com>
2015-01-05 10:30:25 -0500

    pretty print `hrpg todos` commands

--- hrpg/core.py
+++ hrpg/core.py
@@ -6,17 +6,22 @@ Phil Adams http://philadams.net
 
 hrpg: commandline interface for http://habitrpg.com
 http://github.com/philadams/hrpg
+
+TODO: pretty format task-ids
+TODO: handle ranges and lists for done/undone
+TODO: figure out cache solution (shelve-json?) and how/when to invalidate
 """
 
 
 import json
 import os
+from collections import defaultdict
 from pprint import pprint
 
 from docopt import docopt
 from . import api
 
-VERSION = 'hrpg version 0.0.4'
+VERSION = 'hrpg version 0.0.5'
 CONFIG_FILE = '~/.hrpgrc'
 CACHE_FILE = '~/.hrpg.cache'
 
@@ -35,31 +40,6 @@ def load_config(fname):
     return config
 
 
-def cache(key, data):
-    """cache (short term!) data in file, accessible at key"""
-    cached = {}
-    try:
-        cached = json.load(open(os.path.expanduser(CACHE_FILE), 'r'))
-    except IOError as err:
-        pass  # the cache file might not exist, and that's okay
-    except ValueError as err:
-        print('Malformed cache file at %s\n' % CONFIG_FILE)
-        raise ValueError(err.msg)
-    cached[key] = data
-    try:
-        out = open(os.path.expanduser(CACHE_FILE), 'w')
-        out.write(json.dumps(cached))
-        out.close()
-    except IOError as err:
-        raise(IOError('Problem writing to cache file at %s\n' % CACHE_FILE))
-
-
-def from_cache(key):
-    """return data from cache file by key.
-    if key doesn't exist, cache error."""
-    return json.load(open(os.path.expanduser(CACHE_FILE), 'r'))[key]
-
-
 def clear_cache():
     """delete the cache file CACHE_FILE."""
     try:
@@ -67,6 +47,16 @@ def clear_cache():
     except OSError:
         pass  # overwhelmingly, the cache file wasn't there
     print('cache file (%s) deleted' % CACHE_FILE)
+
+
+def get_task_id(args, key='<task-id>'):
+    return int(args[key]) - 1 if args[key] is not None else -1
+
+
+def print_task_list(tasks):
+    for i, task in enumerate(tasks):
+        completed = 'x' if task['completed'] else ' '
+        print('[%s] %s %s' % (completed, i + 1, task['text']))
 
 
 def cli():
@@ -131,64 +121,57 @@ def cli():
 
     # GET/POST habits
     elif args['habits']:
+        tid = get_task_id(args)
+        habits = hbt.user.tasks(type='habit')
         if args['up']:
-            habits = from_cache('habits')
-            cache_id = int(args['<task-id>'])
-            res = hbt.user.tasks(_id=habits[cache_id]['id'],
-                                 _direction='up', _method='post')
-            print('success!')
+            hbt.user.tasks(_id=habits[tid]['id'],
+                           _direction='up', _method='post')
+            print('incremented!')
         elif args['down']:
-            habits = from_cache('habits')
-            cache_id = int(args['<task-id>'])
-            res = hbt.user.tasks(_id=habits[cache_id]['id'],
-                                 _direction='down', _method='post')
-            print('success!')
+            hbt.user.tasks(_id=habits[tid]['id'],
+                           _direction='down', _method='post')
+            print('decremented!')
         else:
-            habits = hbt.user.tasks(type='habit')
-            cache('habits', habits)
-            pprint([e['text'] for e in habits])
+            for i, task in enumerate(habits):
+                pprint(task); exit()
+                # TODO figure out strength to show here
+                strength = 'x' if task['completed'] else ' '
+                print('[%s] %s %s' % (completed, i + 1, task['text']))
 
     # GET/PUT tasks:daily
     elif args['dailies']:
+        tid = get_task_id(args)
+        dailies = hbt.user.tasks(type='daily')
         if args['done']:
-            dailies = from_cache('dailies')
-            cache_id = int(args['<task-id>'])
-            # you'd think this'd work, but you don't get rewards!
-            #res = hbt.user.tasks(_id=dailies[cache_id]['id'],
-            #                     _method='put', completed=True)
-            res = hbt.user.tasks(_id=dailies[cache_id]['id'],
-                                 _direction='up', _method='post')
+            hbt.user.tasks(_id=dailies[tid]['id'],
+                           _direction='up', _method='post')
             print('success!')
         elif args['undo']:
-            dailies = from_cache('dailies')
-            cache_id = int(args['<task-id>'])
-            res = hbt.user.tasks(_id=dailies[cache_id]['id'],
-                                 _method='put', completed=False)
+            hbt.user.tasks(_id=dailies[tid]['id'],
+                           _method='put', completed=False)
             print('success!')
         else:
-            dailies = hbt.user.tasks(type='daily')
-            cache('dailies', dailies)
-            pprint([e['text'] for e in dailies])
+            for i, task in enumerate(dailies):
+                completed = 'x' if task['completed'] else ' '
+                print('[%s] %s %s' % (completed, i + 1, task['text']))
 
     # GET tasks:todo
     elif args['todos']:
+        tid = get_task_id(args)
+        todos = [e for e in hbt.user.tasks(type='todo')
+                if not e['completed']]
         if args['done']:
-            todos = from_cache('todos')
-            cache_id = int(args['<task-id>'])
-            # you'd think this'd work, but you don't get rewards!
-            #res = hbt.user.tasks(_id=todos[cache_id]['id'],
-            #                     _method='put', completed=True)
-            res = hbt.user.tasks(_id=todos[cache_id]['id'],
-                                 _direction='up', _method='post')
-            print('marked complete')
+            hbt.user.tasks(_id=todos[tid]['id'],
+                           _direction='up', _method='post')
+            print('marked todo \'%s\' complete' % todos[tid]['text'])
+            del(todos[tid])
         elif args['add']:
-            res = hbt.user.tasks(type='todo', text=args['<task>'],
-                                 _method='post')
-            print('added new todo')
-        else:
-            todos = hbt.user.tasks(type='todo')
-            cache('todos', todos)
-            pprint([e['text'] for e in todos if not e['completed']])
+            ttext = args['<task>']
+            hbt.user.tasks(type='todo', text=ttext,
+                           _method='post')
+            todos.insert(0, {'completed': False, 'text': ttext})
+            print('added new todo \'%s\'' % ttext)
+        print_task_list(todos)
 
 
 if __name__ == '__main__':

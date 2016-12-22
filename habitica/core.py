001a1ad4a2ca222e1af1ba4190383ea3f8eaeb11
Phil Adams <philadams.net@gmail.com>
2016-12-21 19:14:54 -0500

    tweak spacing in --help

--- habitica/core.py
+++ habitica/core.py
@@ -219,19 +219,19 @@ def cli():
       -c --checklists   Toggle displaying checklists on or off
 
     The habitica commands are:
-      status                 Show HP, XP, GP, and more
-      habits                 List habit tasks
-      habits up <task-id>    Up (+) habit <task-id>
-      habits down <task-id>  Down (-) habit <task-id>
-      dailies                List daily tasks
-      dailies done           Mark daily <task-id> complete
-      dailies undo           Mark daily <task-id> incomplete
-      todos                  List todo tasks
-      todos done <task-id>   Mark one or more todo <task-id> completed
-      todos add <task>       Add todo with description <task>
-      todos delete <task-id> Delete one or more todo <task-id>
-      server                 Show status of Habitica service
-      home                   Open tasks page in default browser
+      status                  Show HP, XP, GP, and more
+      habits                  List habit tasks
+      habits up <task-id>     Up (+) habit <task-id>
+      habits down <task-id>   Down (-) habit <task-id>
+      dailies                 List daily tasks
+      dailies done            Mark daily <task-id> complete
+      dailies undo            Mark daily <task-id> incomplete
+      todos                   List todo tasks
+      todos done <task-id>    Mark one or more todo <task-id> completed
+      todos add <task>        Add todo with description <task>
+      todos delete <task-id>  Delete one or more todo <task-id>
+      server                  Show status of Habitica service
+      home                    Open tasks page in default browser
 
     For `habits up|down`, `dailies done|undo`, and `todos done`, you can pass
     one or more <task-id> parameters, using either comma-separated lists or
